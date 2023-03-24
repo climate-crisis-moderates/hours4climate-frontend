5e881470c5b1380a8723ada133cb9516498d06cb
John Doe <johndoe84449@gmail.com>
2023-03-24 07:31:43 +0100

    Improved

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,78 @@
 <script lang="ts">
-	import { Button, Col, Row, FormGroup, Input } from 'sveltestrap';
-	import Select from 'svelte-select';
-	import HCaptcha from 'svelte-hcaptcha';
+	import { onMount, onDestroy } from "svelte";
+	import { Col, Row, FormGroup, Input } from "sveltestrap";
+	import Select from "svelte-select";
+	import HCaptcha from "svelte-hcaptcha";
+	import Nested from "../logo.svelte";
 
-	async function coutries(text: string) {
-		let url =
-			text == ''
-				? 'https://restcountries.com/v3.1/all/?fields=name'
-				: 'https://restcountries.com/v3.1/name/' + text + '?fields=name';
+	const endpoint = "http://localhost:3000";
 
-		return await fetch(url)
-			.then((response) => response.json())
-			.then((data) => {
-				return data.map((item: any) => item['name']['common']);
-			});
+	let countries: Array<string> = [];
+
+	let combined_hours: Array<Array<string>> = [];
+
+	async function fetchData() {
+		combined_hours = await fetch(endpoint + "/api/summary").then(
+			(response) => response.json()
+		);
+		console.log(combined_hours);
 	}
-	import Nested from '../logo.svelte';
+
+	const pollData = () => {
+		setTimeout(() => {
+			fetchData().then(() => pollData());
+		}, 1000 * 10);
+	};
+
+	onMount(() => {
+		pollData();
+	});
+
+	onDestroy(() => {
+		clearTimeout(pollData);
+	});
+
+	async function get_countries(text: string) {
+		let base = endpoint + "/api/country";
+		let url = text == "" ? base : base + "?name=" + text;
+
+		countries = await fetch(url).then((response) => response.json());
+
+		return countries;
+	}
 
 	let captcha: HCaptcha;
 
 	const handleError = () => {
 		captcha.reset();
 	};
-	let mySitekey = '8458f785-ae1a-492f-b14f-3fdcac064c38';
+	let hcaptcha_key = "10000000-ffff-ffff-ffff-000000000001";
+	// let hcaptcha_key = '8458f785-ae1a-492f-b14f-3fdcac064c38';
 
-	const handleSuccess = () => {
-		console.log('Success!');
+	let token: string | null = null;
+	let country: any = null;
+	let hours: string | null = null;
+
+	let isHuman = (event: any) => {
+		token = event["detail"]["token"];
 	};
+
+	async function pedge() {
+		return await fetch(endpoint + "/api/pledge", {
+			method: "POST",
+			body: JSON.stringify({
+				token,
+				country: country["value"],
+				hours,
+			}),
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+		}).catch((error) => {
+			console.log(error);
+		});
+	}
 </script>
 
 <svelte:head>
@@ -40,69 +86,125 @@
 	<div class="text-center">
 		<Nested class="img-responsive center-block" />
 	</div>
-	<h1 class="text-center">hours4climate</h1>
-	<h2 class="text-center">Work less without disclosing it to your employer, for climate</h2>
+	<h1 class="text-center">
+		Work less without disclosing it to your employer, for climate
+	</h1>
 
-	<p>This page contains a civil desobidence action with a very high impact to cost ratio.</p>
+	<p>A cost-effective civil desobidence action that many can.</p>
 
-	<p>Because:</p>
+	<p>
+		You decide how much to act based on your risk tolerance, personal
+		situation and your perception of your employer's contribution to the
+		climate crisis.
+	</p>
+
+	<p>
+		We suggest 30 minutes a day as a starting point, and adjust to your
+		liking on a month basis.
+	</p>
+
+	<p>
+		Let us know how much you are acting so we can quantify our combined
+		impact:
+	</p>
+
+	<form on:submit|preventDefault={pedge} class="needs-validation">
+		<Row>
+			<Col>
+				<Select
+					placeholder="Country you work at"
+					loadOptions={get_countries}
+					bind:value={country}
+					required
+				/>
+			</Col>
+			<Col>
+				<FormGroup>
+					<Input
+						type="number"
+						placeholder="hours/week"
+						min="0"
+						max="10"
+						step="0.1"
+						bind:value={hours}
+						required
+					/>
+				</FormGroup>
+			</Col>
+			<Col>
+				<button
+					disabled={token === null ||
+						hours === null ||
+						country === null}
+					data-placement="top"
+					title={token === null ? "Fill form" : ""}
+					type="submit"
+					class="btn btn-success"
+					data-toggle="tooltip"
+				>
+					Pledge
+				</button>
+			</Col>
+		</Row>
+		<div class="text-center">
+			<HCaptcha
+				bind:this={captcha}
+				sitekey={hcaptcha_key}
+				on:success={isHuman}
+				on:error={handleError}
+			/>
+		</div>
+	</form>
+	<p>
+		<small>We only store information you provide in the form.</small>
+	</p>
+
+	<p>Why:</p>
 	<ul>
 		<li>
-			your career is not negatively impacted: leverage this free time for training / up-skill
-			yourself
-		</li>
-		<li>it reduces CO2 emissions by reducing economical output</li>
-		<li>
-			it reduces inequality by reducing the amount of value that your employer extracts from you
+			your career is not negatively impacted if you leverage this free
+			time for training / up-skill yourself
 		</li>
 		<li>
-			it impacts companies whose employees believe are contributing the most to the climate crisis
+			<strong>decreases CO2 emissions</strong> by reducing economical output
+		</li>
+		<li>
+			<strong>decreases inequality</strong> by reducing the amount of value
+			that your employer extracts from you
+		</li>
+		<li>
+			<strong>impacts poluting companies the most</strong> as employees know
+			are contributing the most to the climate crisis
 		</li>
 	</ul>
 
-	<p>
-		You decide how much to act based on your risk tolerance, personal situation and your perception
-		of your employer's contribution to the climate crisis.
-	</p>
-	<p>
-		We suggest 30 minutes a day as a starting point, and adjust to your liking on a month basis.
-	</p>
+	<table class="table">
+		<thead>
+			<tr>
+				<th scope="col">Country</th>
+				<th scope="col">Hours/week</th>
+				<th scope="col">Persons</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each combined_hours as country_hours}
+				<tr>
+					<td>{country_hours[0]}</td>
+					<td>{country_hours[1]}</td>
+					<td>{country_hours[2]}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 
-	<p>Let us know how much you are acting so we can quantify our combined impact:</p>
-
-	<Row>
-		<Col>
-			<Select placeholder="Country you work at" loadOptions={coutries} />
-		</Col>
-		<Col>
-			<FormGroup>
-				<Input type="number" placeholder="hours/week" />
-			</FormGroup>
-		</Col>
-		<Col>
-			<Button color="success" outline>Pledge</Button>
-		</Col>
-	</Row>
-	<p>
-		<small>
-			We do not track you. We do not store cookies. We only store the information provided in this
-			form.
-		</small>
-	</p>
-	<div class="text-center">
-		<HCaptcha
-			bind:this={captcha}
-			sitekey={mySitekey}
-			on:success={handleSuccess}
-			on:error={handleError}
-		/>
+	<div class="alert alert-warning" role="alert">
+		TODO: show number of hours per country
 	</div>
-
-	<div class="alert alert-warning" role="alert">TODO: show number of hours per country</div>
-	<div class="alert alert-warning" role="alert">TODO: show total $ loss per month</div>
+	<div class="alert alert-warning" role="alert">
+		TODO: show total $ loss per month
+	</div>
 
 	<footer>
 		<p>Improve us on Github</p>
 	</footer>
 </main>
-<link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" />
