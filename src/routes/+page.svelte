ae6299854558081d5f284883e2bd94e99447e77a
John Doe <johndoe84449@gmail.com>
2023-05-12 05:10:18 +0000

    Added twitter link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount, onDestroy } from "svelte";
 	import Discord from "svelte-bootstrap-icons/lib/Discord.svelte";
+	import Twitter from "svelte-bootstrap-icons/lib/Twitter.svelte";
 
 	import Nested from "../logo.svelte";
 	import Form from "../form.svelte";
@@ -57,10 +58,47 @@
 		style:background-color="#CFE2FF"
 	>
 		<div class="container-fluid">
-			<span class="navbar-brand">Hours4Climate</span>
-			<a href="https://discord.gg/FgcWVbypqU" title="Join us on Discord">
-				<Discord color="#7289da" width={32} height={32} />
-			</a>
+			<div class="d-flex flex-grow-1">
+				<span class="w-100 d-lg-none d-block">
+					<!-- hidden spacer to center brand on mobile --></span
+				>
+				<span class="navbar-brand">Hours4Climate</span>
+				<div class="w-100 text-right">
+					<button
+						class="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#myNavbar7"
+					>
+						<span class="navbar-toggler-icon" />
+					</button>
+				</div>
+			</div>
+			<div
+				class="collapse navbar-collapse flex-grow-1 text-right"
+				id="myNavbar7"
+			>
+				<ul class="navbar-nav ms-auto flex-nowrap">
+					<li class="nav-item">
+						<a
+							class="nav-link"
+							href="https://twitter.com/hours4climate"
+							title="Follow us on twitter"
+						>
+							<Twitter color="#1DA1F2" width={32} height={32} />
+						</a>
+					</li>
+					<li class="nav-item">
+						<a
+							class="nav-link"
+							href="https://discord.gg/FgcWVbypqU"
+							title="Join us on Discord"
+						>
+							<Discord color="#7289da" width={32} height={32} />
+						</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</nav>
 
