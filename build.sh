709ba4ea5c09105225e38908c5eeb98b4740d8a5
John Doe <johndoe84449@gmail.com>
2023-05-13 06:23:51 +0000

    Improved build

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ set -e
 rm -rf _build/
 docker build -t build .
 docker cp $(docker create --name tc build):/home/app/build ./_build && docker rm tc
-rm -rf build/ && mv _build/ build/
+rm -rf build/* && mv _build/* build/
