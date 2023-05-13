e0130b3f8b4be3d5c24a64437e172dc0dd3adc86
John Doe <johndoe84449@gmail.com>
2023-05-13 06:21:10 +0000

    Moved medium to header

---
+++ src/navbar.svelte
@@ -0,0 +1,57 @@
+<script>
+    import Discord from "svelte-bootstrap-icons/lib/Discord.svelte";
+    import Twitter from "svelte-bootstrap-icons/lib/Twitter.svelte";
+
+    import Logo from "./logo.svelte";
+    import MediumLogo from "./medium_logo.svelte";
+</script>
+
+<div class="container-fluid">
+    <div class="d-flex flex-grow-1">
+        <span class="w-100 d-lg-none d-block">
+            <!-- hidden spacer to center brand on mobile --></span
+        >
+        <span class="navbar-brand">Hours4Climate</span>
+        <div class="w-100 text-right">
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#myNavbar7"
+            >
+                <span class="navbar-toggler-icon" />
+            </button>
+        </div>
+    </div>
+    <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar7">
+        <ul class="navbar-nav ms-auto flex-nowrap">
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="https://medium.com/@climate_crisis_moderates/introduction-to-hours4climate-5371456ada93"
+                    title="Check the blog post"
+                >
+                    <MediumLogo />
+                </a>
+            </li>
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="https://twitter.com/hours4climate"
+                    title="Follow us on twitter"
+                >
+                    <Twitter color="#1DA1F2" width={32} height={32} />
+                </a>
+            </li>
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="https://discord.gg/FgcWVbypqU"
+                    title="Join us on Discord"
+                >
+                    <Discord color="#7289da" width={32} height={32} />
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import { onMount, onDestroy } from "svelte";
-	import Discord from "svelte-bootstrap-icons/lib/Discord.svelte";
-	import Twitter from "svelte-bootstrap-icons/lib/Twitter.svelte";
 
-	import Nested from "../logo.svelte";
+	import Logo from "../logo.svelte";
+	import NavBar from "../navbar.svelte";
 	import Form from "../form.svelte";
 	import Benefits from "../benefits.svelte";
 
@@ -62,49 +61,7 @@
 		class="navbar navbar-expand-lg navbar-light"
 		style:background-color="#CFE2FF"
 	>
-		<div class="container-fluid">
-			<div class="d-flex flex-grow-1">
-				<span class="w-100 d-lg-none d-block">
-					<!-- hidden spacer to center brand on mobile --></span
-				>
-				<span class="navbar-brand">Hours4Climate</span>
-				<div class="w-100 text-right">
-					<button
-						class="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#myNavbar7"
-					>
-						<span class="navbar-toggler-icon" />
-					</button>
-				</div>
-			</div>
-			<div
-				class="collapse navbar-collapse flex-grow-1 text-right"
-				id="myNavbar7"
-			>
-				<ul class="navbar-nav ms-auto flex-nowrap">
-					<li class="nav-item">
-						<a
-							class="nav-link"
-							href="https://twitter.com/hours4climate"
-							title="Follow us on twitter"
-						>
-							<Twitter color="#1DA1F2" width={32} height={32} />
-						</a>
-					</li>
-					<li class="nav-item">
-						<a
-							class="nav-link"
-							href="https://discord.gg/FgcWVbypqU"
-							title="Join us on Discord"
-						>
-							<Discord color="#7289da" width={32} height={32} />
-						</a>
-					</li>
-				</ul>
-			</div>
-		</div>
+		<NavBar />
 	</nav>
 
 	<section
@@ -112,7 +69,7 @@
 		style:padding-bottom="2em"
 		style:background-color="#CFE2FF"
 	>
-		<Nested class="img-responsive center-block text-center" />
+		<Logo class="img-responsive center-block text-center" />
 		<h1>Work less without disclosing it</h1>
 		<h4>cost-effective action to tackle the climate crisis</h4>
 	</section>
@@ -215,12 +172,5 @@
 
 	<footer style:padding="2em">
 		<p>Copyright 2023 Hours4Climate</p>
-		<p>
-			<a
-				href="https://medium.com/@climate_crisis_moderates/introduction-to-hours4climate-5371456ada93"
-			>
-				Blog post
-			</a>
-		</p>
 	</footer>
 </main>
